f886cb5a64140816627507eaea4d84e20347457b
Zynh Ludwig <Zynh0722@gmail.com>
2022-02-28 10:17:59 -0800

    Finish Bee.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
 var Bee = function() {
+  Grub.call(this);
+
+  // Override inherited properties
+  this.age = 5;
+  this.color = 'yellow';
+
+  // New properties
+  this.job = 'keep on growing';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
