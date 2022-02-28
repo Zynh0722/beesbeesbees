d738442a46335729ab270f1d7b3db764c9bfb127
Zynh Ludwig <Zynh0722@gmail.com>
2022-02-28 10:18:12 -0800

    Finish HoneyMakerBee.js

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+
+  // Override inherited properties
+  this.age = 10;
+  this.job = 'make honey';
+
+  // New properties
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot += 1;
+};
+
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot -= 1;
+};
